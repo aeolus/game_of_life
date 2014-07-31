09fe0526d35d8c71fba9aec30cc5c6d375b53fac
Chengwei Ding <chengwei.ding@klarna.com>
2014-07-31 16:23:14 +0200

    use maven to configure project

--- src/cellState.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created with IntelliJ IDEA. User: chengwei Date: 30/07/14 Time: 19:17 To
- * change this template use File | Settings | File Templates.
- */
-public enum CellState {
-    ALIVE, DEAD
-}
